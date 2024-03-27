221b62a8d5218a2a1e7baccf2865b799499ed157
SebastianLontis <lontis.sebastian2003@gmail.com>
2024-03-27 16:27:28 +0100

    merge

--- Proiect CV/smekerie.js
+++ Proiect CV/smekerie.js
@@ -1,37 +1,31 @@
 //funtia de schimbare a culorii de fundal
-var changeColorButton = document.getElementById('change-color-button');
-
 var originalBackgroundColor = document.body.style.backgroundColor;
+
+var changeColorButton = document.getElementById('change-color-button');
 
 var navbar = document.querySelector('.navbar');
 
 var turqoisColor = 'rgb(8, 145, 135)';
-
 var darkturqoisColor = 'rgb(0, 100, 100)';
 
-navbar.style.backgroundColor = darkturqoisColor;
+navbar.style.backgroundColor = originalBackgroundColor;
+
 var isTurquoise = false;
 
 changeColorButton.addEventListener('click', function() {
-    
     if (!isTurquoise) {
-        // Schimbăm culoarea fundalului paginii
         document.body.style.backgroundColor = turqoisColor;
-        // Schimbăm culoarea fundalului navbar-ului
         navbar.style.backgroundColor = darkturqoisColor;
-        // Actualizăm starea pentru turcoaz
         isTurquoise = true;
     } else {
-        // Schimbăm culoarea fundalului paginii înapoi la cea originală
         document.body.style.backgroundColor = originalBackgroundColor;
-        // Schimbăm culoarea fundalului navbar-ului înapoi la cea originală
         navbar.style.backgroundColor = originalBackgroundColor;
-        // Actualizăm starea curentă
         isTurquoise = false;
     }
 });
 
-//funtia de schimbare a font size
+
+//funtia de scroll to top si scroll to bottom
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY || window.pageYOffset;
     var scrollHeight = document.documentElement.scrollHeight;
@@ -47,22 +41,49 @@ window.addEventListener('scroll', function() {
     
   });
   
-  
-  function scrollToBottom() {
+  window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY || window.pageYOffset;
+    var scrollHeight = document.documentElement.scrollHeight;
+    var clientHeight = document.documentElement.clientHeight;
+
+    if (scrollPosition > (scrollHeight - clientHeight) / 2) {
+       
+        document.getElementById('scroll-to-bottom').style.display = 'none';
+        document.getElementById('scroll-to-top').style.display = 'block';
+    } else {
+        
+        document.getElementById('scroll-to-bottom').style.display = 'block';
+        document.getElementById('scroll-to-top').style.display = 'none';
+    }
+});
+
+// Functia pentru a derula pagină în partea de jos
+function scrollToBottom() {
     window.scrollTo({
         top: document.body.scrollHeight,
         behavior: 'smooth'
     });
-  }
-  
-  function scrollToTop() {
+}
+
+// Functia pentru a derula pagină în partea de sus
+function scrollToTop() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
-  }
-  
-  // function changeBackgroundColor(sectionId, color) {
-  //   var section = document.getElementById(sectionId);
-  //   section.style.backgroundColor = color;
-  // }
+}
+
+window.addEventListener('load', function() {
+    document.getElementById('scroll-to-bottom').style.display = 'block';
+    document.getElementById('scroll-to-top').style.display = 'none';
+});
+
+
+
+function updateName() {
+    let titleNameH1= document.getElementById("nume");
+    const newNameH1=prompt("Introduceti numele dumneavoastra: ");
+    if(newNameH1!==null && newNameH1!==""){
+        titleNameH1.innerHTML=newNameH1;
+    }
+}
